94767938e3cd882318c3a2c43d9d8a5fe97d8f7e
VitorRT_WSL <vitorrt2015@gmail.com>
2023-11-17 21:50:03 -0300

    feat: add random number generator

--- 2-Programming_a_Guessing_Game/guessing_game/src/main.rs
+++ 2-Programming_a_Guessing_Game/guessing_game/src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number!");
 
     println!("Please input your guess.");
@@ -12,4 +16,11 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
