69c36ae7054370d3fde70ff5112326b81aefcad2
VitorRT_WSL <vitorrt2015@gmail.com>
2023-12-15 20:53:20 -0300

    feat: add generic

---
+++ 10-Generics/generics/src/main.rs
@@ -0,0 +1,54 @@
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Rectangle<T> {
+    width: T,
+    height: T,
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let rectangle = Rectangle { width: 22.2 , height: 1.2 };
+}
