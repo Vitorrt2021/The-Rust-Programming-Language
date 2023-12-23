634d6d60fb6330b356d749271d7c8fa067f169ce
VitorRT_WSL <vitorrt2015@gmail.com>
2023-12-22 22:17:26 -0300

    feat: add file read

--- 12-Command-Line-Program/minigrep/src/main.rs
+++ 12-Command-Line-Program/minigrep/src/main.rs
@@ -1,13 +1,48 @@
 use std::env;
+use std::fs;
+use std::process;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    run(config);
 }
 
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = &args[1];
+        let file_path = &args[2];
+    
+        dbg!(&args);
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        println!("Searching for {}", query);
+        println!("In file {}", file_path);
+        println!("Current folder {:?}", env::current_dir().unwrap());
+    
+        Ok(Config { query, file_path })
+    }
+}
