97ee7ffcd8b47a0eab12183b1def0a7623a85884
VitorRT_WSL <vitorrt2015@gmail.com>
2023-12-24 15:06:41 -0300

    feat: add env variable

--- 12-Command-Line-Program/minigrep/src/lib.rs
+++ 12-Command-Line-Program/minigrep/src/lib.rs
@@ -4,11 +4,17 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-   
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
     println!("================================================================");
     println!("========================== Result ==============================");
 
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{line}");
     }
 
@@ -20,6 +26,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub  struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -34,12 +41,13 @@ impl Config {
         dbg!(&args);
         let query = args[1].clone();
         let file_path = args[2].clone();
-    
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         println!("Searching for {}", query);
         println!("In file {}", file_path);
         println!("Current folder {:?}", env::current_dir().unwrap());
     
-        Ok(Config { query, file_path })
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
@@ -48,6 +56,22 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -68,5 +92,20 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- 12-Command-Line-Program/minigrep/src/main.rs
+++ 12-Command-Line-Program/minigrep/src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(config) {
-        println!("Problem running: {err}");
+        eprintln!("Problem running: {err}");
         process::exit(1);
     }
 }
