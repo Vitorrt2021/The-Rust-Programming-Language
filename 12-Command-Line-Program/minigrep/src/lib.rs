98474e5bb7edd6bd4966afa5b4a43be87dfe114b
VitorRT_WSL <vitorrt2015@gmail.com>
2023-12-23 21:43:18 -0300

    feat: add tests

---
+++ 12-Command-Line-Program/minigrep/src/lib.rs
@@ -0,0 +1,72 @@
+use std::env;
+use std::fs;
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+   
+    println!("================================================================");
+    println!("========================== Result ==============================");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    println!("================================================================");
+
+    Ok(())
+}
+
+pub  struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = &args[1];
+        let file_path = &args[2];
+    
+        dbg!(&args);
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        println!("Searching for {}", query);
+        println!("In file {}", file_path);
+        println!("Current folder {:?}", env::current_dir().unwrap());
+    
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- 12-Command-Line-Program/minigrep/src/main.rs
+++ 12-Command-Line-Program/minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
 
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,38 +11,8 @@ fn main() {
         process::exit(1);
     });
 
-    run(config);
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = &args[1];
-        let file_path = &args[2];
-    
-        dbg!(&args);
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        println!("Searching for {}", query);
-        println!("In file {}", file_path);
-        println!("Current folder {:?}", env::current_dir().unwrap());
-    
-        Ok(Config { query, file_path })
+    if let Err(err) = minigrep::run(config) {
+        println!("Problem running: {err}");
+        process::exit(1);
     }
 }
