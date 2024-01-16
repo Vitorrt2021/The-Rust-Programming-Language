15ac1d898720f1c4a4198a0b15010a8bede9a815
VitorRT_WSL <vitorrt2015@gmail.com>
2024-01-16 07:51:19 -0300

    feat: patterns

--- 18-Patterns/patterns/src/main.rs
+++ 18-Patterns/patterns/src/main.rs
@@ -3,8 +3,73 @@ fn main() {
     while_let();
     for_loops();
     function_parameters();
+    multiple_match();
 }
 
+fn multiple_match() {
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        4..=5 => println!("four through five"),
+        _ => println!("anything"),
+    }
+
+    let s = 'z';
+    match s {
+        'a'..='j' => println!("early ASCII letter"),
+        _ => println!("end of ASCII letter"),
+    }
+
+
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
+        None => (),
+    }
+
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
 
 fn if_let(){
     let favorite_color: Option<&str> = None;
